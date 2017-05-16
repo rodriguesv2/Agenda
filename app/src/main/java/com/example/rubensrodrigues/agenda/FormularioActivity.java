217563bd572a5c552957b7bd8ac545934f1d0c42
Rubens Rodrigues <Rubens Rodrigues>
2017-05-15 21:56:40 -0300

    Primeiro Commit

---
+++ app/src/main/java/com/example/rubensrodrigues/agenda/FormularioActivity.java
@@ -0,0 +1,28 @@
+package com.example.rubensrodrigues.agenda;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class FormularioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario);
+
+        Button botaoSalvar = (Button) findViewById(R.id.formulario_salvar);
+        botaoSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(FormularioActivity.this, "Botao clicado!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+}
