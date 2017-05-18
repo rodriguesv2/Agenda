9d6f260ddef089747ce3648bb4085ac7876c0485
Rubens Rodrigues <rubens_v2@hotmail.com>
2017-05-17 23:56:20 -0300

    Criação de classe AlunoDAO - Windows 10

---
+++ app/src/main/java/com/example/rubensrodrigues/agenda/dao/AlunoDAO.java
@@ -0,0 +1,30 @@
+package com.example.rubensrodrigues.agenda.dao;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Rubens Rodrigues on 17/05/2017.
+ */
+
+public class AlunoDAO extends SQLiteOpenHelper{
+
+    public AlunoDAO(Context context) {
+        super(context, "Agenda", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String sql = "CREATE TABLE Alunos (id INTEGER PRIMARY KEY; nome TEXT NOT NULL, endereco TEXT, telefone TEXT, site TEXT, " +
+                "nota REAL;";
+        db.execSQL(sql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        String sql = "DROP TABLE IF EXISTS Alunos";
+        db.execSQL(sql);
+        onCreate(db);
+    }
+}
