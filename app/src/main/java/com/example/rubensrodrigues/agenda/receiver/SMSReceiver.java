6549d58d0cd72cba1f2b119b48aa43532a74b98d
Rubens Rodrigues <rubens_v2@hotmail.com>
2017-06-16 17:47:30 -0300

    Tratamento de SMS - Ubuntu

---
+++ app/src/main/java/com/example/rubensrodrigues/agenda/receiver/SMSReceiver.java
@@ -0,0 +1,35 @@
+package com.example.rubensrodrigues.agenda.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.telephony.SmsMessage;
+import android.widget.Toast;
+
+import com.example.rubensrodrigues.agenda.R;
+import com.example.rubensrodrigues.agenda.dao.AlunoDAO;
+
+/**
+ * Created by rubens on 16/06/17.
+ */
+
+public class SMSReceiver extends BroadcastReceiver{
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Object[] pdus = (Object[]) intent.getSerializableExtra("pdus");
+        byte[] pdu = (byte[]) pdus[0];
+        String formato = (String) intent.getSerializableExtra("format");
+
+        SmsMessage sms = SmsMessage.createFromPdu(pdu, formato);
+
+        String telefone = sms.getDisplayOriginatingAddress();
+        AlunoDAO dao = new AlunoDAO(context);
+        if(dao.ehAluno(telefone)) {
+            Toast.makeText(context, "Chegou um SMS de Aluno!", Toast.LENGTH_SHORT).show();
+            MediaPlayer mp = MediaPlayer.create(context, R.raw.msg);
+            mp.start();
+        }
+        dao.close();
+    }
+}
