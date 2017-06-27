bb313b6086932e0901dd8332f5e74d26d4cc60c1
Rubens Rodrigues <rubens_v2@hotmail.com>
2017-06-27 15:55:23 -0300

    Nova tela "Provas" - Ubuntu

---
+++ app/src/main/java/com/example/rubensrodrigues/agenda/ProvasActivity.java
@@ -0,0 +1,44 @@
+package com.example.rubensrodrigues.agenda;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.example.rubensrodrigues.agenda.modelo.Prova;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ProvasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_provas);
+
+        List<String> topicosPort = Arrays.asList("Sujeito", "Objeto direto", "Objeto indireto");
+        Prova provaPortugues = new Prova("Portugues","25/05/2016", topicosPort);
+
+        List<String> topicosMat = Arrays.asList("Equacoes de segundo grau", "Trigonometria");
+        Prova provaMatematica = new Prova("Matematica","27/05/2016", topicosMat);
+
+        List<Prova> provas = Arrays.asList(provaPortugues, provaMatematica);
+
+        ArrayAdapter<Prova> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, provas);
+
+        ListView lista = (ListView) findViewById(R.id.provas_lista);
+        lista.setAdapter(adapter);
+
+        lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long l) {
+                Prova prova = (Prova)parent.getItemAtPosition(position);
+                Toast.makeText(ProvasActivity.this, "Clicou na prova de " + prova, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
