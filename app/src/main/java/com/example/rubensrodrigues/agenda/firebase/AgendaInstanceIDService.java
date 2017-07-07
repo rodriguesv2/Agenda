aa1098a08f6ebe90d1dbdda65075e0fa99833936
Rubens Rodrigues <rubens_v2@hotmail.com>
2017-07-07 17:30:48 -0300

    Notificação com o Firebase (falhando) - Ubuntu

---
+++ app/src/main/java/com/example/rubensrodrigues/agenda/firebase/AgendaInstanceIDService.java
@@ -0,0 +1,28 @@
+package com.example.rubensrodrigues.agenda.firebase;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by rubens on 07/07/17.
+ */
+
+public class AgendaInstanceIDService extends FirebaseInstanceIdService{
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d("Token firebase", "Refreshed token: " + refreshedToken);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        enviaTokenParaServidor(refreshedToken);
+    }
+
+    private void enviaTokenParaServidor(String refreshedToken) {
+
+    }
+}
